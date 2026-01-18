0458eb4e5774ed083a6d651eb95dc18ff5c1aa1b
Matthew Que <matthew.neale.que@gmail.com>
2026-01-17 22:07:51 -0500

    feat(csharp): fix issue with Fory varuint32 encoding

--- csharp/Fory/Fory.Core.Tests/Encoding/ForyEncodingTests.cs
+++ csharp/Fory/Fory.Core.Tests/Encoding/ForyEncodingTests.cs
@@ -1,26 +1,33 @@
-﻿using Fory.Core.Encoding;
+﻿using System.IO.Pipelines;
+using Fory.Core.Encoding;
 
 namespace Fory.Core.Tests.Encoding;
 
 public class ForyEncodingTests
 {
     [Theory]
-    [InlineData(0, new byte[] { 0 })] // 1-byte min value
-    [InlineData(127, new byte[] { 127 })] // 1-byte max value
-    [InlineData(128, new byte[] { 128, 1 })] // 2-byte min value
-    [InlineData(16383, new byte[] { 255, 127 })] // 2-byte max value
-    [InlineData(16384, new byte[] { 128, 128, 1 })] // 3-byte min value
-    [InlineData(2097151, new byte[] { 255, 255, 127 })] // 3-byte max value
-    [InlineData(2097152, new byte[] { 128, 128, 128, 1 })] // 4-byte min value
-    [InlineData(268435455, new byte[] { 255, 255, 255, 127 })] // 4-byte max value
-    [InlineData(268435456, new byte[] { 128, 128, 128, 128, 1 })] // 5-byte min value
-    [InlineData(4294967295, new byte[] { 255, 255, 255, 255, 15 })] // 5-byte max value/unsigned int max value
-    public void Should_Encode_VarInt32(uint input, byte[] output)
+    [InlineData(0)] // 1-byte min value
+    [InlineData(127)] // 1-byte max value
+    [InlineData(128)] // 2-byte min value
+    [InlineData(16383)] // 2-byte max value
+    [InlineData(16384)] // 3-byte min value
+    [InlineData(2097151)] // 3-byte max value
+    [InlineData(2097152)] // 4-byte min value
+    [InlineData(268435455)] // 4-byte max value
+    [InlineData(268435456)] // 5-byte min value
+    [InlineData(4294967295)] // 5-byte max value/unsigned int max value
+    public async Task Should_Encode_Decode_VarInt32(uint input)
     {
+        // Arrange
+        var pipe = new Pipe();
+
         // Act
-        var result = ForyEncoding.AsVarUInt32(input).ToArray();
+        var byteResult = ForyEncoding.AsVarUInt32(input);
+        await pipe.Writer.WriteAsync(byteResult);
+        await pipe.Writer.CompleteAsync();
+        var result = await ForyEncoding.FromVarUInt32Async(pipe.Reader);
 
         // Assert
-        Assert.Equal(output, result);
+        Assert.Equal(input, result);
     }
 }
